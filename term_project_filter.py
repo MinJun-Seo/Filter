18f2dc588bbc8860b5261bf22386b5f0a97e7412
MinJun-Seo <seo241736@gmail.com>
2024-12-07 18:01:45 +0900

    실시간 사람인식 기능 추가

---
+++ term_project_filter.py
@@ -0,0 +1,33 @@
+import cv2
+
+cascade_path = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+
+face_cascade = cv2.CascadeClassifier(cascade_path)
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: 카메라를 열 수 없습니다.")
+    exit()
+
+print("ESC 키를 눌러 종료하세요.")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: 프레임을 읽을 수 없습니다.")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow('Face Detection', frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
